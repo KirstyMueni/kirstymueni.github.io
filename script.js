3a7d8fa0192be65618583f78c45207df39dfcece
Kirsty Mueni <kirstymueni@gmail.com>
2026-07-01 17:05:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -297,8 +297,8 @@ const galleryData = {
         description: 'Developed Math Quiz Challenge ; an interactive educational game developed in Scratch that helps learners strengthen their arithmetic skills through engaging, game-based learning. The application presents multiple mathematics questions, evaluates user responses in real time, provides immediate feedback, and tracks the learner's score using variables and conditional logic. Designed to promote computational thinking and digital literacy, the project demonstrates the use of Scratch programming to create an interactive and enjoyable classroom learning experience'
       },
       {
-        name: 'Coding Workshop',
-        src: '',
+        name: 'PhET Interactive Simulations',
+        src: 'Integrated PhET Interactive Simulations into STEM to provide learners with virtual experiments in Physics, Chemistry, Biology, and Mathematics, enhancing conceptual understanding through inquiry-based and hands-on digital learning.',
         description: 'Interactive coding and programming classes'
       },
       {
